e2e983a2dcc11df183a3cb6adbe11038e89bd68c
Damola Adediran <tylher123@gmail.com>
2022-07-14 20:09:46 +0100

    Add custom method to Comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   scope :update_post, ->(id, text) { find_by(id:).update(text:) }
-  
+
   def most_recent_comments
     comments.order(created_at: :desc).first(5)
   end
