c21b8b5e6ab727ecae5d34759f278a93d16f0991
Damola Adediran <tylher123@gmail.com>
2022-07-14 22:58:13 +0100

    Add post_counter update to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,11 @@ class Post < ApplicationRecord
 
   scope :update_post, ->(id, text) { find_by(id:).update(text:) }
 
+  def update_post_count
+    new_count = author.posts.count
+    author.update(posts_counter: new_count)
+  end
+
   def most_recent_comments
     comments.order(created_at: :desc).first(5)
   end
