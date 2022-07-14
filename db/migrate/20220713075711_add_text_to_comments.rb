4728f31566164f17e3d693ff7ac85cc8f3199f8f
Damola Adediran <tylher123@gmail.com>
2022-07-14 15:03:38 +0100

    Correct migration files

--- db/migrate/20220713075711_add_text_to_comments.rb
+++ db/migrate/20220713075711_add_text_to_comments.rb
@@ -1,5 +1,5 @@
 class AddTextToComments < ActiveRecord::Migration[7.0]
   def change
-    add_column :comments, :text, :string
+    add_column :comments, :text, :text
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_13_080023) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "post_id", null: false
-    t.string "text"
+    t.text "text"
     t.bigint "author_id", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
@@ -34,8 +34,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_13_080023) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "text"
+    t.text "title"
+    t.text "text"
     t.integer "comments_counter"
     t.integer "likes_counter"
     t.datetime "created_at", null: false
@@ -45,9 +45,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_13_080023) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "photo"
-    t.string "bio"
+    t.text "name"
+    t.text "photo"
+    t.text "bio"
     t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
