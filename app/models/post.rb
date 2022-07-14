e54a2ede011264df07351d31ace5ec13a0d8e7b5
Damola Adediran <tylher123@gmail.com>
2022-07-14 19:29:49 +0100

    Add custom method to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
-    has_many :comments
-    has_many :likes
-    belongs_to :author, class_name: "User", foreign_key: "author_id"
+  has_many :comments
+  has_many :likes
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  scope :update_post, ->(id, text) { find_by(id:).update(text:) }
+  # Ex:- scope :active, -> {where(:active => true)}
 end
