640a1e51caf77107c2d830d0f24e97de6f66f381
Damola Adediran <tylher123@gmail.com>
2022-07-15 18:33:33 +0100

    write validation tests for post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,5 +8,21 @@ RSpec.describe Post, type: :model do
         subject.title = nil
         expect(subject).to be_valid
     end
+
+    it "title should not exceed 250 characters" do
+       subject.title =  "ybisscougiqwfcw7ioubmgcoihkbshpjphsdhplksbjgdugubvhsdvdcygkbjopjpbjh
+        ihijldchiubvajstfhjdscoi[k;jhuksdclkn,j hoil nvhuldp0ipkoiyuidsf6usfopjpguiguvhjvkhss
+        cjpcudchidiogougjduigddkudcijbjcknkln;cjiildcj,bdbcygukvhvdcudgoiucjbsdvcyfsye5fgkjdc
+        nlkohkldviodhvln.dcidhoinkldnvihdiohvilndknvidhidkdlnviodhcoindck.n.kdcokjbskxjbkjbax
+        lnalxnknlnsclnllllllllllllllllaknxjlabxkblboihlnklnnklnlkanxjbagyxgjmxghvcchxgcghavxjgv
+        bkxkhbaxgiuuga axnghvhjavxkjbmam xjhbkjxhbkjhvxxjgvjvkabxlnkjbaxhvavwukbbsckjsbcjbscbbkjsb
+        kubkbbsaxkjksgkvsxkuhsolxb,jslkxhlsbkjhulkhskkx,bsxjgixysx lksj;cjpohgcaxvkaxakxjjbakjxbhjvjaxh
+        khvksxjhhkgkskhklsxsxiugsucouugosxkslxnjbkbaxgch ajxgakvkhsbksixuusgxisxhhvsxsxygsixgsx
+        kuubkscjnkscschvsciuscibsckjsbksvjsbksbsvcsvgvhgcaxbakxbkjaxhbbajvjaxhvigisxvgsjusgusx
+        jgjgvgjsxkjnskxksglsliihslsxlihsxysisjxjhgsjxhbsjxvghscxhgsxhsxgchsxusgxugusxysxusuxyyggs
+        kukusxgugsxguihgisxyfsysxhgchassxvjsxsjvjsxxsiuisxsjkkxiusxsufusxsuxygyusxyfsutxsxvjsxj
+        ygisxxiuhsgxiusxyfsuxsxvsxugksclisscouscgusf8duwd98td7[owdp9wd90ywiwfysisyfikbsciysaascsutasaclsacyuf"
+        expect(subject).to_not be_valid  
+    end
     
 end
