b4467018105aa3876dbe3c497db6642dd52c158a
Damola Adediran <tylher123@gmail.com>
2022-07-16 01:18:39 +0100

    Fix linters

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,15 +4,7 @@ RSpec.describe Post, type: :model do
   subject { Post.new(author: User.new(name: 'Wale', posts_counter: 0), title: 'Work in progress') }
   before { subject.save }
 
-  context 'validation tests' do
-    it 'title should not be blank' do
-      subject.title = nil
-      subject.comments_counter = 0
-      subject.likes_counter = 0
-      expect(subject).to_not be_valid
-    end
-    it 'title should not exceed 250 characters' do
-      subject.title = "ybisscougiqwfcw7ioubmgcoihkbshpjphsdhplksbjgdugubvhsdvdcygkbjopjpbjh
+  test_str = "ybisscougiqwfcw7ioubmgcoihkbshpjphsdhplksbjgdugubvhsdvdcygkbjopjpbjh
         ihijldchiubvajstfhjdscoi[k;jhuksdclkn,j hoil nvhuldp0ipkoiyuidsf6usfopjpguiguvhjvkhss
         cjpcudchidiogougjduigddkudcijbjcknkln;cjiildcj,bdbcygukvhvdcudgoiucjbsdvcyfsye5fgkjdc
         nlkohkldviodhvln.dcidhoinkldnvihdiohvilndknvidhidkdlnviodhcoindck.n.kdcokjbskxjbkjbax
@@ -24,6 +16,16 @@ RSpec.describe Post, type: :model do
         jgjgvgjsxkjnskxksglsliihslsxlihsxysisjxjhgsjxhbsjxvghscxhgsxhsxgchsxusgxugusxysxusuxyyggs
         kukusxgugsxguihgisxyfsysxhgchassxvjsxsjvjsxxsiuisxsjkkxiusxsufusxsuxygyusxyfsutxsxvjsxj
         ygisxxiuhsgxiusxyfsuxsxvsxugksclisscouscgusf8duwd98td7[owdp9wd90ywiwfysisyfikbsciysaascsutasaclsacyuf"
+
+  context 'validation tests' do
+    it 'title should not be blank' do
+      subject.title = nil
+      subject.comments_counter = 0
+      subject.likes_counter = 0
+      expect(subject).to_not be_valid
+    end
+    it 'title should not exceed 250 characters' do
+      subject.title = test_str
       subject.comments_counter = 0
       subject.likes_counter = 0
       expect(subject).to_not be_valid
@@ -41,7 +43,6 @@ RSpec.describe Post, type: :model do
   end
 
   context 'model methods tests' do
-    
     user1 = User.new(name: 'taiwo', posts_counter: 0)
     user1.save
     Post.create(author: user1, title: 'hello', likes_counter: 0, comments_counter: 0)
@@ -50,11 +51,10 @@ RSpec.describe Post, type: :model do
     post4 = Post.create(author: user1, title: 'Hahahaha', likes_counter: 0, comments_counter: 0)
     post4.update_post_count
 
-
     it 'should return the updated post count' do
       expect(user1.posts_counter).to eq 4
     end
   end
-    Post.destroy_all
-    User.destroy_all
+  Post.destroy_all
+  User.destroy_all
 end
