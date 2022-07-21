9083ba3f1eb82632839695114896ac0426a02624
Damola Adediran <tylher123@gmail.com>
2022-07-21 23:43:06 +0100

    Connect user model to user views through contoller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
+
+  root "users#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
