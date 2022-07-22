ce35aaf3b353574f8202df463735689aa802dfc0
Damola Adediran <tylher123@gmail.com>
2022-07-22 02:35:08 +0100

    Create variable to hold post data for post view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
-  def show; end
+  def show
+    @post =  Post.find(params[:id])
+  end
 
   def index
     @posts = Post.where(author_id: params[:user_id])
