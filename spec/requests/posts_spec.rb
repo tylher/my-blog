399ffa5275436a0d96bf1bc1bc9ab5dcf48c16a0
Damola Adediran <tylher123@gmail.com>
2022-07-26 14:20:31 +0100

    Add features test for user

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  before(:all) do
+    user1 = User.new(id:1,name: 'taiwo', posts_counter: 0)
+    user1.save
+    Post.create(id:1,author: user1, title: 'hello',text:'jkbnidbcuouewcuiguowece', likes_counter: 0, comments_counter: 0)
+    Post.create(id:2,author: user1, title: 'what you doing',text:'jkbnidbcuouewcuiguowece', likes_counter: 0, comments_counter: 0)
+    Post.create(id:3,author: user1, title: 'amazing post',text:'jkbnidbcuouewcuiguowece', likes_counter: 0, comments_counter: 0)
+    Post.create(id:4,author: user1, title: 'Hahahaha',text:'jkbnidbcuouewcuiguowece', likes_counter: 0, comments_counter: 0)
+  end
   describe 'GET /users/posts' do
     before(:each) { get user_posts_path(1) }
     it 'works! (now write some real specs)' do
