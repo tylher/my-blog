91d6a865c175e4af92e61dfd0e31322d631b439f
Damola Adediran <tylher123@gmail.com>
2022-07-27 08:37:03 +0100

    Correc database structure

--- Gemfile
+++ Gemfile
@@ -57,6 +57,11 @@ gem "image_processing", "~> 1.2"
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'rails-controller-testing'
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'selenium-webdriver'
+  gem 'webdrivers'
 end
 
 group :development do
@@ -77,7 +82,7 @@ group :test do
   gem 'database_cleaner'
 end
 
-group :development, :test do
-  gem 'capybara'
-  gem 'rspec-rails'
-end
+# group :development, :test do
+#   gem 'capybara'
+#   gem 'rspec-rails'
+# end
--- spec/views/users/users_spec.rb
+++ spec/views/users/users_spec.rb
@@ -2,6 +2,10 @@ RSpec.describe "GET/ users", type: :feature do
     before(:all) do
          @user=User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',posts_counter: 0)
          @user1=User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.', posts_counter: 0)
+
+         @post1=Post.create(title:'power above all', text:'we are all pencils in the hand of the creator',author:@user )
+         @post2=Post.create(title:'Buisness', text:'Never being lazy',author:@user )
+         @post2.update_post_count
     end
     describe "user index" do
         it "description" do
@@ -11,12 +15,14 @@ RSpec.describe "GET/ users", type: :feature do
    end
    it "description" do
      visit '/' 
-     expect(page.first("img[src='https://unsplash.com/photos/F_-0BxGuVvo']")['alt']).to have_content(@user.name)
-     page.find('img[alt=Lilly]')
+     expect(page).to have_css('img[alt=Tom]')
+     expect(page).to have_css('img[alt=Lilly]')
+
    end
    it "description" do
     visit '/'
-    expect(page).to have_css('p', text:'Number of posts:', count:3)  
+    expect(page).to have_css('p', text:'Number of posts: 2') 
+    expect(page).to have_css('p', text:'Number of posts: 0') 
    end
 
    it "description" do
@@ -29,21 +35,21 @@ RSpec.describe "GET/ users", type: :feature do
 
     describe "show page" do
         it "description" do
-            visit '/users/2'
+            visit "/users/#{@user.id}"
         expect(page).to have_css("img[src='https://unsplash.com/photos/F_-0BxGuVvo']")
         end
         
         it "description" do
-           visit '/users/2' 
+           visit "/users/#{@user.id}" 
            expect(page).to have_content(@user.name)
         end
-        it "shows " do
-           visit '/users/2' 
+        it "shows user's bio" do
+           visit "/users/#{@user.id}"  
            expect(page).to have_content(@user.bio)
         end
         it "Shows number of posts" do
-           visit '/users/2' 
-           expect(page).to have_content(@user.posts_counter)
+           visit "/users/#{@user.id}" 
+           expect(page).to have_content('Number of posts: 2')
         end
         
     end
