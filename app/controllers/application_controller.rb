69a88a1879079dd7266398eed4d2eeb9d5e3a727
Damola Adediran <tylher123@gmail.com>
2022-07-28 11:48:35 +0100

    Add devise email confirmation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,10 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    # devise_parameter_sanitizer.permit(:confirmation) { |u| u.permit(:name, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :surname, :email, :password, :current_password)
     end
+
   end
 end
