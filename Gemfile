b312b90d44a24dc7bb0f60301e17d3cb45f96bf7
unknown <tylher123@gmail.com>
2023-07-07 13:14:09 +0100

    set up render

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 gem 'rubocop', '>= 1.0', '< 2.0'
 
-ruby '3.1.2'
+ruby File.read(".ruby-version").strip
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.3'
@@ -67,7 +67,7 @@ end
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
-  gem 'web-console'
+  # gem 'web-console'
 
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,21 +1,29 @@
 class Post < ApplicationRecord
   has_many :comments
+
   has_many :likes
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  belongs_to :author, class_name: "User", foreign_key: "author_id"
 
   # scope :update_post, ->(id, text) { find_by(id:).update(text:) }
 
   validates :likes_counter, :comments_counter, numericality: { only_integer: true, greater_or_equal_to: 0 }
+
   validates :title, presence: true, length: { maximum: 250 }
 
-  def update_post_count
-    new_count = author.posts.count
-    author.update(posts_counter: new_count)
-  end
+  # def update_post_count
+
+  #   new_count = author.posts.count
+
+  #   author.update(posts_counter: new_count)
+
+  # end
 
   def most_recent_comments
     comments.order(created_at: :desc).first(5)
   end
 
   after_create :update_post_count
+
+  scope :update_post_count, -> { author.posts_counter += 1 }
 end
