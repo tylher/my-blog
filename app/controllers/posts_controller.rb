6ba50fcee68360121e256720d737bbfa27506934
unknown <tylher123@gmail.com>
2023-08-02 21:02:09 +0100

    build homepage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def show
-    @post = Post.find(params[:id])
+    @post = Post.find_by(title: params[:title])
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
   get "/login", to: "login#form"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create] do
+    resources :posts, only: %i[index new create] do
       resources :comments, only: %i[new create]
       resources :likes, only: %i[new create]
     end
   end
+
+  get "/posts/:title", to: "posts#show", :as => :post
 
   root "home#index"
   # Defines the root path route ("/")
