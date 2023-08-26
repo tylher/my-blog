b14c6a045bc414ee37828f97bfcd7ed01346c92a
unknown <tylher123@gmail.com>
2023-08-26 02:13:16 +0100

    Revert "Update post cards"
    This reverts commit 46bab208883fc8523d3e54b420ad23887082cb9a.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_params, if: :devise_controller?
   include Pagy::Backend
-
-  def configure_permitted_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio])
-  end
 end
