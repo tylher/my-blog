42abcb402bed553e37114005e539986c2668e699
unknown <tylher123@gmail.com>
2023-09-11 21:20:59 +0100

    Update readme file

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: "author_id"
   has_many :likes, foreign_key: "author_id"
   validates :name, presence: true
+  has_one_attached :avatar
+
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def most_recent_posts
